e0d727423a05989bc3522f61709491beecc5809c
Esra Altuntas <altuntasesra14@gmail.com>
2023-03-12 00:10:54 +0100

    update my second java class

--- src/first_project/MySecondClass.java
+++ src/first_project/MySecondClass.java
@@ -3,6 +3,6 @@ package first_project;
 public class MySecondClass {
     public static void main(String[] args) {
         System.out.println("This is new code");
-
+        System.out.println("UPDATING CODE");
     }
 }
